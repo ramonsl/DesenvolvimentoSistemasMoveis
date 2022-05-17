a0e84a5067fb11b426b43e19048c88b03432afe5
Ramon Lummertz <ramonsl@gmail.com>
2022-05-17 17:32:46 -0300

    Shared prefers

---
+++ shared_prefs/lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('shared_preferences'),
+      ),
+      body: Center(
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: _read,
+                style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 40.0, vertical: 20.0),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0)),
+                    primary: Colors.purple),
+                child: Text(
+                  'Ler',
+                  style: const TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: _save,
+                style: ElevatedButton.styleFrom(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+                  primary: Colors.deepPurpleAccent,
+                  shape: StadiumBorder(),
+                ),
+                child: Text(
+                  "Gravar",
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _read() async {
+    final prefs = await SharedPreferences.getInstance();
+    const key = 'my_int_key';
+    final value = prefs.getInt(key) ?? 0;
+    print('read: $value');
+  }
+
+  _save() async {
+    final prefs = await SharedPreferences.getInstance();
+    const key = 'my_int_key';
+    const value = 1000;
+    prefs.setInt(key, value);
+    print('saved $value');
+  }
+}
